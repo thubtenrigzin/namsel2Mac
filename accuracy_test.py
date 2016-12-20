ae7e0473686009e0956488eff43558b3a7e576a6
Zach Rowinski <zach.rowinski@gmail.com>
2016-12-19 16:22:22 -0800

    Update accuracy_test.py

--- accuracy_test.py
+++ accuracy_test.py
@@ -33,13 +33,14 @@ keys = tsets.keys()
 keys.sort()
 all_samples = []
 
+## Baseline accuracies for the data in tsets
 baseline = [0.608, 0.5785123966942148, 0.4782608695652174, 0.7522123893805309, 
             0.6884057971014492, 0.5447154471544715, 0.9752066115702479, 
             0.9830508474576272]
 
 
-def test_accuracy(t, clsf=None, verbose=True):
-    
+def test_accuracy(t, clsf=None):
+    '''Get accuracy score for a testset t'''
     if clsf:
         cls = clsf
     else:
@@ -54,11 +55,9 @@ def test_accuracy(t, clsf=None, verbose=True):
         x3.append(normalize_and_extract_features(j))
     
     pred = cls.predict(x3)
-#    
+
     s  = 0
     for i, p in enumerate(pred):
-##        print p, i
-#        pr = np.argmax(p)
         if float(p) == y[i]:
             s += 1.0            
         else:
@@ -68,7 +67,9 @@ def test_accuracy(t, clsf=None, verbose=True):
     score = s / len(y)
     return score
 
-def test_all(clsf=None, verbose=False):
+def test_all(clsf=None):
+    '''Run accuracy tests for all testsets'''
+    
     print 'starting tests. this will take a moment'
     
     test_accuracy(keys[0], clsf)
@@ -82,4 +83,4 @@ def test_all(clsf=None, verbose=False):
     return np.mean(all_samples)
 
 if __name__ == '__main__':
-    print test_all(verbose=True)
+    print test_all()
