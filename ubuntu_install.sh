6654dca2ffa37fe879d40b690b795461ddb040eb
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-07-29 15:11:57 +0200

    fix https://github.com/zmr/namsel/issues/4

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,15 +1,10 @@
 #! /usr/bin/env bash
 # Do all of this and then save a snapshot
-sudo apt-get update
-sudo apt-get install python-pip
-sudo apt-get install build-essential python-dev python-setuptools \
-                     python-numpy python-scipy \
-                     libatlas-dev libatlas3gf-base
-sudo apt-get install python-opencv libtiff-tools python-gtk2 python-cairo unzip
-sudo apt-get install ghostscript
-sudo apt-get install scantailor
-sudo pip install Cython
-sudo pip install pillow sklearn requests simplejson
+sudo apt-get install python-pip python2.7-dev \
+                     build-essential python-setuptools \
+                     libatlas-dev libatlas3-base ghostscript scantailor \
+                     python-opencv libtiff-tools python-gtk2 python-cairo unzip
+sudo pip2 install Cython pillow sklearn requests simplejson wheel scipy numpy
 python setup.py build_ext --inplace
 mkdir -p ~/.fontscp data_generation/fonts/*ttf ~/.fonts/
 fc-cache -f -v
