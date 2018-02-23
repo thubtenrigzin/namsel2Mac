e05b98ab34ad618aeea12bee97da2c074ccd8af7
Thubtenrigzin ཐུབ་བསྟན་རིག་འཛིན། <thubtenrigzin@users.noreply.github.com>
2018-02-23 10:41:09 +0800

    Dowgrade scikit-learn to 0.18.1 version
    The latest version of scikit-learn seems to not work with Namsel.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -5,6 +5,7 @@ sudo apt-get install python-pip python2.7-dev \
                      libatlas-dev libatlas3-base ghostscript scantailor \
                      python-opencv libtiff-tools python-gtk2 python-cairo unzip
 sudo pip2 install Cython pillow sklearn requests simplejson wheel scipy numpy
+sudo pip2 install --upgrade scikit-learn==0.18.1
 python setup.py build_ext --inplace
 mkdir -p ~/.fontscp data_generation/fonts/*ttf ~/.fonts/
 fc-cache -f -v
